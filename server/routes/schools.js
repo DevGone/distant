03fa72a10e952a8e98a68b0095ea5917b01bf849
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-15 16:07:54 +0100

    added /schools/ and /schools/:slug

--- server/routes/schools.js
+++ server/routes/schools.js
@@ -2,24 +2,27 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var parseUrlEncoded = bodyParser.urlencoded({ extended: false });
-
-var schools = [];
+var slug = require('../helpers/slug');
+var model = require('../data/model');
 
 /* GET schools listing. */
 router.route('/')
   .get(function(request, response) {
-
+    response.json(model.getAllSchools());
   })
   .post(parseUrlEncoded, function(request, response) {
 
   });
 
 router.route('/:slug')
-  .all(function(request, response) {
-
-  })
   .get(function(request, response) {
-
+    var schoolSlug = request.params.slug;
+    var school = model.getSchool(schoolSlug);
+    if (school) {
+      response.json(school);
+    } else {
+      response.status(404).send('School not found');
+    }
   });
 
 module.exports = router;
