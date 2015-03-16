ea0dfc8f6548e22732d3c9365f821a1db26e1eb5
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-16 15:20:42 +0100

    teacher app with school list

--- public/scripts/teacher.js
+++ public/scripts/teacher.js
@@ -2,4 +2,35 @@
 
   var app = angular.module('teacher', ['ngRoute']);
 
+  app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'partials/home',
+      controller: 'HomeController'
+    })
+    .when('/schools/', {
+      templateUrl: 'partials/schools',
+      controller: 'SchoolListController'
+    })
+  });
+
+  app.controller('HomeController', function($scope) {
+    var self = $scope;
+
+    self.title = 'Bonjour !'
+  });
+
+  app.controller('SchoolListController', ['$http', '$scope', function($http, $scope) {
+    var self = this;
+    $scope.self = self;
+
+    self.title = 'Schools !'
+
+    self.schools = [];
+
+    $http.get('/schools/').success(function(data) {
+      self.schools = data;
+    });
+
+  }]);
+
 })();
